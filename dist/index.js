90b0c8b26e3e881d618f11d6c99f5c912e7ac76b
Abraham Przewodnik <a.przewodnik@yellowtree.de>
2021-04-15 16:39:48 +0200

    Change dist from ESNext to ES5

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,11 @@
-export const fit = (mode, parentDimensions, childDimensions, options) => {
-    const { focusPoint = { x: 0.5, y: 0.5 }, extraOffset = { left: 0, top: 0 }, roundSize = true, allowUpscaling = true } = options || {};
-    const childRatio = childDimensions.height / childDimensions.width;
-    const parentRatio = parentDimensions.height / parentDimensions.width;
-    const alignByWidth = (mode === 'cover' && childRatio > parentRatio) ||
+export var fit = function (mode, parentDimensions, childDimensions, options) {
+    var _a = options || {}, _b = _a.focusPoint, focusPoint = _b === void 0 ? { x: 0.5, y: 0.5 } : _b, _c = _a.extraOffset, extraOffset = _c === void 0 ? { left: 0, top: 0 } : _c, _d = _a.roundSize, roundSize = _d === void 0 ? true : _d, _e = _a.allowUpscaling, allowUpscaling = _e === void 0 ? true : _e;
+    var childRatio = childDimensions.height / childDimensions.width;
+    var parentRatio = parentDimensions.height / parentDimensions.width;
+    var alignByWidth = (mode === 'cover' && childRatio > parentRatio) ||
         (mode === 'contain' && childRatio < parentRatio) ||
         mode === 'width';
-    let size = alignByWidth
+    var size = alignByWidth
         ? {
             width: parentDimensions.width,
             height: roundSize ? Math.ceil(parentDimensions.width * childRatio) : parentDimensions.width * childRatio
@@ -14,21 +14,21 @@ export const fit = (mode, parentDimensions, childDimensions, options) => {
             width: roundSize ? Math.ceil(parentDimensions.height / childRatio) : parentDimensions.height / childRatio,
             height: parentDimensions.height
         };
-    const scale = size.width / childDimensions.width;
+    var scale = size.width / childDimensions.width;
     if (!allowUpscaling && scale > 1) {
         size = {
             width: childDimensions.width,
             height: childDimensions.height
         };
     }
-    const offset = {
+    var offset = {
         left: (parentDimensions.width - size.width) * focusPoint.x + extraOffset.left,
         top: (parentDimensions.height - size.height) * focusPoint.y + extraOffset.top
     };
     return {
-        size,
-        offset,
-        scale
+        size: size,
+        offset: offset,
+        scale: scale
     };
 };
 //# sourceMappingURL=index.js.map
